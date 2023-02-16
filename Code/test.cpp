86e53fd3f7f51a4bbedfba01b2e2f009c4159b96
Ali Jafari <alijfri99@gmail.com>
2023-02-16 10:25:04 +0330

    Tested the adapted domains and problems.

--- Code/test.cpp
+++ Code/test.cpp
@@ -6,36 +6,13 @@
 #include "Utils/SetUtils.h"
 #include "Model/Action.h"
 #include "Domains/DepotsDomain.h"
+#include "Problems/TireProblem.h"
+#include "Problems/LinkRepeatProblem.h"
+#include "Planners/BackwardPlanner.h"
 
 int main()
 {
-    /*Predicate p1(1, {1, 2, 3});
-    Predicate p2(2, {3, 4, 5});
-    Predicate p3(3, {7, 5, 2});
-    Predicate p4(4, {7, 9, 3});
-    Predicate p5(1, {1, 2, 3});
-    std::cout << (p1 == p1) << " " << (p1 == p5) << " " << (p1 < p2) << " " << (p3 < p2) << std::endl;
-    std::cout << (p2 == p3) << std::endl;
-    State s(NULL, "", {p2}, {p3, p4});
-    Action a("Attach(B, C)", {p1, p3}, {}, {p1, p2}, {});
-    std::cout << a.is_relevant(s) << std::endl;
-    State s2 = a.regress(&s);
-    std::unordered_set<State> uset = {s, s2};
-    std::set<Predicate> s1 = {p1};
-    std::set<Predicate> s2 = {p3, p5};
-    bool sub = SetUtils::is_subset(s1, s2);
-    TireProblem *tire_problem = new TireProblem(new TireDomain());
-    Planner *back = new BackwardPlanner(tire_problem);
-    std::list<std::string> result = back->search();
-    delete back;
-    int b = 3;*/
-
-    Predicate p2(2, {3, 4, 5});
-    Predicate p3(3, {7, 5, 2});
-    Predicate p4(4, {7, 9, 3});
-    Predicate p5(1, {1, 2, 3});
-    std::cout << "GO!" << std::endl;
-    Domain *depots = new DepotsDomain(2, 2, 1);
-    std::cout << "DONE!" << std::endl;
-    int sdf = 2;
+    BackwardPlanner back(new LinkRepeatProblem(new LinkRepeatDomain(100)));
+    std::vector<std::string> actions = back.search();
+    int a = 2;
 }
